50793b681a012cd28745aea633cba183a3273728
Kevin.Wright2 <kevin.wright@fdmgroup.com>
2020-02-28 15:19:52 +0000

    dummy change

--- Corona/src/main/java/com/fdmgroup/Corona/CoronaApplication.java
+++ Corona/src/main/java/com/fdmgroup/Corona/CoronaApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CoronaApplication {
 
+	// Dummy change
+	// Another dummy change
 	public static void main(String[] args) {
 		SpringApplication.run(CoronaApplication.class, args);
 	}
