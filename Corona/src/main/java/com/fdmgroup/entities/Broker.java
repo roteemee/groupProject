77a925c0b8e5f911c8685b0f3fcbab29ad98b732
Samuel Oni <Samuel.Oni@fdmgroup.com>
2020-03-02 15:11:33 +0000

    Add Sysadmin and Shareholder

---
+++ Corona/src/main/java/com/fdmgroup/entities/Broker.java
@@ -0,0 +1,20 @@
+package com.fdmgroup.entities;
+
+import javax.persistence.Entity;
+
+@Entity(name="brokers")
+public class Broker extends User{
+	
+	protected void acceptRequest() {
+		
+	}
+	
+	protected void denyRequest() {
+		
+	}
+	
+	protected void updateFee() {
+		
+	}
+	
+}
---
+++ Corona/src/main/java/com/fdmgroup/entities/Sysadmin.java
@@ -0,0 +1,26 @@
+package com.fdmgroup.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity(name="sysadmin")
+public class Sysadmin {
+	
+	public void listTrades() {
+		
+	}
+	
+	public void listUsers() {
+		
+	}
+	
+	public void setFee() {
+		
+	}
+	
+	public void updateFee() {
+		
+	}
+
+}
