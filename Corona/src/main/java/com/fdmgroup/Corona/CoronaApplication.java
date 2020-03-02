10b90ee1056c6587d855f8e696355329194594fa
Kevin.Wright2 <kevin.wright@fdmgroup.com>
2020-03-02 08:34:00 +0000

    removed dummy comment

--- Corona/src/main/java/com/fdmgroup/Corona/CoronaApplication.java
+++ Corona/src/main/java/com/fdmgroup/Corona/CoronaApplication.java
@@ -8,7 +8,7 @@ public class CoronaApplication {
 
 	
 	
-	// This is another change made from home
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CoronaApplication.class, args);
 	}
