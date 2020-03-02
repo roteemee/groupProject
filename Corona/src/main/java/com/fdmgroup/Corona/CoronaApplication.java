cc89415b9d35aa7d86c58d096627958e618353b9
Kevin.Wright2 <kevin.wright@fdmgroup.com>
2020-03-02 14:34:52 +0000

    modify Application to configure where repositories and entities are held.

--- Corona/src/main/java/com/fdmgroup/Corona/CoronaApplication.java
+++ Corona/src/main/java/com/fdmgroup/Corona/CoronaApplication.java
@@ -2,8 +2,13 @@ package com.fdmgroup.Corona;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "your.repositories.package")
+@EntityScan(basePackages = "your.entities.package")
+
 public class CoronaApplication {
 	
 	public static void main(String[] args) {
