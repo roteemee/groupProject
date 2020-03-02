80309c016e0dac7e53274097cfe6c7152714501e
Wenyu.Wu <wenyu.wu@fdmgroup.com>
2020-03-02 16:19:57 +0000

    Create login and register webpage

--- Corona/src/main/java/com/fdmgroup/Corona/HomeController.java
+++ Corona/src/main/java/com/fdmgroup/Corona/HomeController.java
@@ -11,4 +11,14 @@ public class HomeController {
 		return "home";
 	}
 	
+	@GetMapping("/login")
+	public String login() {
+		return "login";
+	}
+	
+	@GetMapping("/register")
+	public String register() {
+		return "register";
+	}
+	
 }
