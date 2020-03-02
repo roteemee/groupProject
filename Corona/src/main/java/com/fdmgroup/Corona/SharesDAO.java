8a5c09e88c9bb3fe745a4555cbb8f61ff675fcc6
Mark Gyimah <Mark.Gyimah@fdmgroup.com>
2020-03-02 14:13:10 +0000

    Add DAO classes

--- Corona/src/main/java/com/fdmgroup/Corona/SharesDAO.java
+++ Corona/src/main/java/com/fdmgroup/Corona/SharesDAO.java
@@ -4,7 +4,8 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.stereotype.Service;
+@Service
 public class SharesDAO {
 	@Autowired
 	private ShareRep repp;
