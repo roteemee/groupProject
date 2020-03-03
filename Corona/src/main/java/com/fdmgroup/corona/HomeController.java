93531ceed941534a44c042413015ff47daabd137
ben.towner <ben.towner@fdmgroup.com>
2020-03-03 11:14:24 +0000

    pulling all changes

--- Corona/src/main/java/com/fdmgroup/corona/HomeController.java
+++ Corona/src/main/java/com/fdmgroup/corona/HomeController.java
@@ -13,67 +13,51 @@ public class HomeController {
 	public String doWork() {
 		return "home";
 	}
-	
+
 	@GetMapping("/login")
 	public String login() {
 		return "login";
 	}
-	
+
 	@GetMapping("/register")
 	public String register() {
 		return "register";
 	}
-	
-<<<<<<< HEAD
 
-=======
->>>>>>> 78a7739175b38d61a155bb4f09113888d24936da
 	@GetMapping("/ViewShares")
 	public String viewShares() {
 		return "ViewShares";
 	}
-	
+
 	@GetMapping("/Shareholder")
 	public String shareholder() {
 		return "Shareholder";
 	}
-	
+
 	@GetMapping("/Broker")
 	public String broker() {
 		return "Broker";
 	}
-	
+
 	@GetMapping("/BrokerRequestPage")
 	public String brokerRequestPage() {
 		return "BrokerRequestPage";
 	}
-	
-<<<<<<< HEAD
 
-=======
->>>>>>> 78a7739175b38d61a155bb4f09113888d24936da
 	@GetMapping("/helloAdmin")
 	public String helloAdmin() {
 		return "helloAdmin";
 	}
-	
-	
-	//user 
+
+	// user
 	@GetMapping("/addUser")
 	public String addUser() {
 		return "addUser";
 	}
-	
-	
-	
+
 	@GetMapping("/manageUser")
 	public String manageUser() {
 		return "manageUser";
 	}
-	
-	
-<<<<<<< HEAD
 
-=======
->>>>>>> 78a7739175b38d61a155bb4f09113888d24936da
 }
