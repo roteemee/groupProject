861425be4d4e1c063300eb83b612a43630849abb
Paulius Juskauskas <Paulius.Juskauskas@fdmgroup.com>
2020-03-03 09:58:02 +0000

    Fix SharesDAO

--- Corona/src/main/java/com/fdmgroup/corona/SharePricesDAO.java
+++ Corona/src/main/java/com/fdmgroup/corona/SharePricesDAO.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.fdmgroup.entities.SharePrice;
 import com.fdmgroup.repos.SharePriceRep;
 @Service
 public class SharePricesDAO {
