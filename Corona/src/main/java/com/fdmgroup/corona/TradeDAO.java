664b628709dc03f743e770ca1d7b807bc438ddd3
Paulius Juskauskas <Paulius.Juskauskas@fdmgroup.com>
2020-03-03 10:24:51 +0000

    Fixed import in TradeDAO

--- Corona/src/main/java/com/fdmgroup/corona/TradeDAO.java
+++ Corona/src/main/java/com/fdmgroup/corona/TradeDAO.java
@@ -6,7 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.fdmgroup.repos.Trade;
+import com.fdmgroup.entities.Trade;
 import com.fdmgroup.repos.TradeRep;
 
 @Service
