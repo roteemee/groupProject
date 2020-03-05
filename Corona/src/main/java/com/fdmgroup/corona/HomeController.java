bbbd52205bb20bfdddc0443bb3ec985b389ae7c8
Olarotimi Akinbote <Olarotimi.Akinbote@fdmgroup.com>
2020-03-05 16:17:30 +0000

    hththt

--- Corona/src/main/java/com/fdmgroup/corona/HomeController.java
+++ Corona/src/main/java/com/fdmgroup/corona/HomeController.java
@@ -1,7 +1,6 @@
 package com.fdmgroup.corona;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,12 +12,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
-
 import com.fdmgroup.entities.BasicUser;
 import com.fdmgroup.entities.Broker;
-import com.fdmgroup.entities.Wallet;
 import com.fdmgroup.entities.Shareholder;
 import com.fdmgroup.entities.UserRequest;
+import com.fdmgroup.entities.Wallet;
 import com.fdmgroup.repos.WalletRep;
 
 @Controller
